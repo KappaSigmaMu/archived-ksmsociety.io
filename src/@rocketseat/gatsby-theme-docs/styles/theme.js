5985baf26b1edc1a62ba8c4a5b43cf19857a41e7
Alan R. Fachini <hi@alfakini.com>
2021-05-16 18:16:57 -0300

    Text review. Add Trello link. Start changing theme

--- src/@rocketseat/gatsby-theme-docs/styles/theme.js
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -1,14 +1,17 @@
 export default {
+  font: {
+    body: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"
+  },
   colors: {
-    primary: `#8257E6`,
-    background: `#000000`,
-    shape: `#F2F2FA`,
-    title: `#3D3D4D`,
-    text: `#6C6C80`,
+    primary: `#fff`,
+    background: `#000`,
+    shape: `#00ffe1`,
+    title: `#eee`,
+    text: `#ddd`,
     components: {
       blockquote: {
-        background: `#feebc8`,
-        text: `#2d3748`,
+        background: `#e6007a`,
+        text: `#fff`,
       },
     },
   },
