0069da51c71700259634ae3f6797dc7fb7c7f2ed
Lauro <lauro@magrathealabs.com>
2021-05-17 19:59:48 -0300

    Change logo to black

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -3,7 +3,7 @@ import React from "react";
 import societyLogo from "./logo.png";
 
 const Logo = () => (
-  <img src={societyLogo} alt="Society logo" style={{ width: 250 }} />
+  <img src={societyLogo} alt="Society logo" style={{ width: 220 }} />
 );
 
 export default Logo;
