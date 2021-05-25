00bafdc3b8ccc71e9833e11599c0f5084de36a29
Alan Fachini <hi@alfakini.com>
2021-05-25 20:54:55 -0300

    Remove theme plugin (#2)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { useStaticQuery, graphql } from 'gatsby';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useStaticQuery, graphql } from "gatsby";
 
 const Container = styled.header`
   display: flex;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,15 +4,15 @@ const theme = {
     header: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;",
   },
   colors: {
-    primary: `#fff`,
-    background: `#000`,
-    shape: `#00ffe1`,
-    title: `#eee`,
-    text: `#ddd`,
+    primary: "#fff",
+    background: "#000",
+    shape: "#00ffe1",
+    title: "#eee",
+    text: "#ddd",
     components: {
       blockquote: {
-        background: `#e6007a`,
-        text: `#fff`,
+        background: "#e6007a",
+        text: "#fff",
       },
     },
   },
