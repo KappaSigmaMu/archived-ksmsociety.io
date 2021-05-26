cec52da063c758bc01a6d93de061a66738037638
Alan Fachini <hi@alfakini.com>
2021-05-25 21:30:33 -0300

    Revert "Remove theme plugin (#2)"
    This reverts commit 00bafdc3b8ccc71e9833e11599c0f5084de36a29.

--- src/@rocketseat/gatsby-theme-docs/components/Sidebar/ExternalLink/index.js
+++ src/@rocketseat/gatsby-theme-docs/components/Sidebar/ExternalLink/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { FiExternalLink } from "react-icons/fi";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FiExternalLink } from 'react-icons/fi';
 
 export default function ExternalLink({ link, label }) {
   return (
     <a href={link} rel="noopener noreferrer">
       {label}
       <FiExternalLink
-        style={{ width: "16px", height: "16px", marginLeft: "10px" }}
+        style={{ width: '16px', height: '16px', marginLeft: '10px' }}
       />
     </a>
   );
